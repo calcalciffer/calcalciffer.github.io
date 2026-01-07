f2c4f7ecbf95709e339f41ecf0604b85873bf0d3
Cal Calcifer <calcalciffer@gmail.com>
2026-01-07 14:23:09 -0500

    fix index

--- pages/home.py
+++ pages/home.py
@@ -3,7 +3,7 @@ from dom_generator_helper import *
 def generate_home_page_content():
     with div(cls="content"):
         with div(cls="container py-4"):
-            h1("Welcome to the Civilization Player League leaderboards!")
+            h1("Welcome to the Civilization Player League leaderboards!", cls='civ-ability-name')
 
 def get_home_page(pages_list):
     return create_page(
